9dfbd64ea071e75f8fcc23e624e4a00a2ad644cb
d-amor <rew1212@naver.com>
2023-02-18 22:56:26 +0900

    update charater

---
+++ Assets/Charater2Move.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charater2Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            this.transform.Translate(-0.05f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(0.05f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0.0f, 0.05f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0.0f, -0.05f, 0.0f);
+        }
+    }
+}
